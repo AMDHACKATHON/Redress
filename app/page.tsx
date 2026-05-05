2e3978c000df6aa4d566be761101e65a3b1db298
SAMKIEL <samuelezekiel488@gmail.com>
2026-05-05 21:46:09 +0100

    feat: initialize Next.js project structure with Tailwind CSS configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Welcome to Redress yippes</h1>
-      </div>
-    </main>
-  );
+  return <main />
 }
