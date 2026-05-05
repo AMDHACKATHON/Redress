d5a4ab5ce08589b83f9df0a715bee168f9cea24b
SAMKIEL <samuelezekiel488@gmail.com>
2026-05-05 21:16:23 +0100

    feat: initialize landing page with welcome heading

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Welcome to Redress</h1>
+        <h1>Welcome to Redress yippes</h1>
       </div>
     </main>
   );
